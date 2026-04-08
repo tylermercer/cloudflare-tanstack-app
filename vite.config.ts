9e1c7129394a988017f752086542a0987df9702a
Tyler Mercer <acct@tylermercer.net>
2026-04-08 12:11:24 -0600

    Fix module imports

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,37 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import cssAutoImport from 'vite-plugin-css-auto-import'
+import path from 'node:path'
 
 const config = defineConfig({
   plugins: [
     cssAutoImport({
       styleExtensions: ['.module.scss'],
       matchComponentName: true,
+      shouldTransformComponent(filename, filepath) {
+        const shouldTransform = !filepath.startsWith('\x00') && filename.includes('.tsx') && !filename.startsWith('__') && (filepath.includes('/routes/') || filepath.includes('/components/'));
+        if (shouldTransform) {
+          console.log({ filename, filepath, shouldTransform });
+        }
+        return Promise.resolve(shouldTransform);
+      },
+      resolveStyleForComponent(_componentName, _directoryName, filePath) {
+        // 1. Remove any query parameters (e.g., ?tsr-split=component)
+        const cleanPath = filePath.split('?')[0];
+
+        // 2. Parse the path to get the directory and the filename without extension
+        const { dir, name } = path.parse(cleanPath);
+
+        // 3. Construct the new path: /original/path/to/filename.module.scss
+        // This ensures it stays in the "same dir" as requested.
+        const stylepath = path.join(dir, `${name}.module.scss`);
+
+        console.log({stylepath, _componentName})
+        return {
+          isModule: true,
+          filePath: stylepath
+        }
+      },
     }),
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
